d3ebd11f55423001c7b9a4a6f2ca3b36bf51fe06
D4ryl00 <d4ryl00@gmail.com>
2025-07-29 16:40:35 +0200

    feat: bump gnonative
    Signed-off-by: D4ryl00 <d4ryl00@gmail.com>

--- mobile/src/grpc/client.ts
+++ mobile/src/grpc/client.ts
@@ -1,11 +1,11 @@
-import { createPromiseClient } from "@connectrpc/connect";
+import { createClient } from "@connectrpc/connect";
 import { createXHRGrpcWebTransport } from "./transport_web";
-import { IndexerService } from "@buf/gnolang_dsocial-indexer.connectrpc_es/indexerservice_connect";
-import { NotificationService } from "@buf/gnolang_dsocial-notification.connectrpc_es/notificationservice_connect";
+import { IndexerService } from "@buf/gnolang_dsocial-indexer.bufbuild_es/indexerservice_pb";
+import { NotificationService } from "@buf/gnolang_dsocial-notification.bufbuild_es/notificationservice_pb";
 
 // Create an indexer client
 export function createIndexerClient(address: string) {
-  return createPromiseClient(
+  return createClient(
     IndexerService,
     createXHRGrpcWebTransport({
       baseUrl: address,
@@ -15,7 +15,7 @@ export function createIndexerClient(address: string) {
 
 // Create an push notitication client
 export function createNotificationClient(address: string) {
-  return createPromiseClient(
+  return createClient(
     NotificationService,
     createXHRGrpcWebTransport({
       baseUrl: address,
